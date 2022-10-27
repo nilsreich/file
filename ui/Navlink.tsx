9c94a4a23b69e769cdf677337dbaeb322ab66449
nilsreich <nilsreich@gmail.com>
2022-10-27 10:21:12 +0000

    Update Navlink.tsx

--- ui/Navlink.tsx
+++ ui/Navlink.tsx
@@ -1,14 +1,15 @@
 'use client';
 
 import Link from 'next/link';
-import { useSelectedLayoutSegments } from "next/navigation";
+import { useSelectedLayoutSegments } from 'next/navigation';
 
 export const Navlink = ({ href, children }) => {
   let segment = useSelectedLayoutSegments();
   let active = href === `/${segment}`;
-  console.log({active, segment})
+
+  console.log({ segment, href });
   return (
-    <Link className={active ? "underline" : ""} href={href}>
+    <Link className={active ? 'underline' : ''} href={href}>
       {children}
     </Link>
   );
